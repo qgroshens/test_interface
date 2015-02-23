92d2770dd6f01e87c85ef7d22d52529c21adb69e
shinversus <quentin.groshens@supelec.fr>
2015-02-23 15:50:08 +0100

    fenetre
    wesh!

--- fenetre_principal.java
+++ fenetre_principal.java
@@ -1,10 +1,21 @@
 package test_interface;
-
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Color;
 public class fenetre_principal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] arg) {	
+		JFrame cadre = new javax.swing.JFrame("Premiere fenetre");
+		JPanel panneau = new JPanel();
+		panneau.setPreferredSize(new Dimension(250, 150));
+
+		panneau.setBackground(Color.RED);
+		cadre.setContentPane(panneau);
+		cadre.setLocation(400, 300);
+		cadre.pack();
+		cadre.setVisible(true);
+		cadre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
-
 }
